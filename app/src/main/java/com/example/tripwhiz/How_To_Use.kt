0e9ea0b1eb552f10114fb2db37ea73844caecc0f
cybertronayush <44187942+cybertronayush@users.noreply.github.com>
2020-10-18 09:58:59 +0530

    here is initial commits containing Source Code For TripWhiz Splash Screen

---
+++ app/src/main/java/com/example/tripwhiz/How_To_Use.kt
@@ -0,0 +1,13 @@
+package com.example.tripwhiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class How_To_Use : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_how__to__use)
+
+        // here i will just implementing Chucks of code for Back button funtionality
+    }
+}
--- app/src/main/java/com/example/tripwhiz/PlanTrip.kt
+++ app/src/main/java/com/example/tripwhiz/PlanTrip.kt
@@ -1,11 +1,25 @@
 package com.example.tripwhiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class PlanTrip : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan_trip)
+
+//Here i will writes those OnclickListner's when i'll work on backend
+            //for testing i am just using sleep try catch here ..........
+        val SplashScreenTimeOut= 7000
+        val howIntent = Intent(this@PlanTrip,How_To_Use::class.java)
+
+        Handler().postDelayed(
+            {
+                startActivity(howIntent)
+                finish()
+            },SplashScreenTimeOut.toLong()
+        )
     }
 }
