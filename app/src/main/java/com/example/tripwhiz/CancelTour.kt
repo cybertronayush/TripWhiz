f2769956249c04fa6f9bf1e421f2fc530d4b5530
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2020-10-19 14:29:20 +0530

    add CancelTour layout

---
+++ app/src/main/java/com/example/tripwhiz/CancelTour.kt
@@ -0,0 +1,11 @@
+package com.example.tripwhiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CancelTour : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cancel_tour)
+    }
+}
