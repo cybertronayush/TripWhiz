6a1f6d6b9309b77f70edbceeb42e75d8e2d69913
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2020-10-21 18:22:59 +0530

    add TourDetails layout

---
+++ app/src/main/java/com/example/tripwhiz/TourDetails.kt
@@ -0,0 +1,12 @@
+package com.example.tripwhiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TourDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tour_details)
+    }
+}
