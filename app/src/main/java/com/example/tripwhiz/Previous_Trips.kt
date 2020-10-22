2b0da019a88157b440929ad891e9bf544c608c5b
cybertronayush <44187942+cybertronayush@users.noreply.github.com>
2020-10-22 13:12:44 +0530

    here is initial commits containing Source Code For TripWhiz Splash Screen

--- app/src/main/java/com/example/tripwhiz/Previous_Trips.kt
+++ app/src/main/java/com/example/tripwhiz/Previous_Trips.kt
@@ -1,20 +1,26 @@
 package com.example.tripwhiz
 
+
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
-class backfeed : AppCompatActivity() {
+class Previous_Trips : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_backfeed)
+        setContentView(R.layout.activity_previous__trips)
         val SplashScreenTimeOut= 1000
-        val prevIntent = Intent(this@backfeed,Previous_Trips::class.java)
+        val DetIntent = Intent(this@Previous_Trips,details_Page::class.java)
 
         Handler().postDelayed(
             {
-                startActivity(prevIntent)
+                startActivity(DetIntent)
 
                 finish()
             },SplashScreenTimeOut.toLong()
